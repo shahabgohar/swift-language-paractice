438224fef7a8d5e08b9a6881f9ba0af6409d5028
shahabgohar <shahab.developer.work@gmail.com>
2022-08-17 22:30:21 +0500

    switch case simple match

---
+++ switch-case.swift
@@ -0,0 +1,12 @@
+let name = "twostraws"
+
+switch name {
+case "bilbo":
+    print("Hello, Bilbo")
+
+case "twostraws":
+    print("Hello, twostraws !")
+    
+default: 
+    print("Authentication fails !")  
+}
