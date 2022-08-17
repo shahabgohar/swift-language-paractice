7373ff9a3ef0fa1700d77bde93d1b6f43a2dba71
shahabgohar <shahab.developer.work@gmail.com>
2022-08-17 22:34:29 +0500

    partial matches in switch case

--- switch-case.swift
+++ switch-case.swift
@@ -1,13 +1,13 @@
 let name = "twostraws"
 let password = "iampassword"
 
-let authentication = (name: "twostraws", password:"iampassword")
+let authentication = (name: "twostraws", password:"iampassword", ipAddress: "127.0.0.1")
 
 switch authentication {
-case ("bilbo", "iampassword"):
+case ("bilbo", "iampassword",_):
     print("Hello, Bilbo")
 
-case ("twostraws", "iampassword"):
+case ("twostraws", "iampassword", _):
     print("Hello, twostraws !")
     
 default: 
