5e811c539b5be03c30aed58838bfe9d1fa2b8343
shahabgohar <shahab.developer.work@gmail.com>
2022-08-18 03:59:07 +0500

    optional chaining in switch case

--- optional/optional.swift
+++ optional/optional.swift
@@ -3,10 +3,10 @@ let password:String? = "iampassword"
 
 
 switch (name, password) {
-case let(.some(name), .some(password)):
+case let(name?, password?):
     print("the name is \(name) with password \(password)")
 
-case let(.some(name), .none):
+case let(name?, nil):
     print("there is no password provided \(name)")
 
 default: 
