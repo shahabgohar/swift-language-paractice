e1b5cf0f056bda2569485468d0b8aa9e76681707
shahabgohar <shahab.developer.work@gmail.com>
2022-08-18 00:03:02 +0500

    simple iteration over array

---
+++ loops/loops.swift
@@ -0,0 +1,9 @@
+let twoStraws = (name: "shahab", password: "iampassword")
+let bilbo = (name: "bilbo", password: "bagg1n5")
+let taylor = (name: "taylor", password: "fr0st1es")
+
+let users = [twoStraws, bilbo, taylor]
+
+for user in users {
+    print(user.name)
+}
