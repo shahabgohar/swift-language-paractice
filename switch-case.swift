5301017fd905c48d968ba6b12a9f76526429c315
shahabgohar <shahab.developer.work@gmail.com>
2022-08-17 22:42:34 +0500

    if you want to know the other part after partial matching

--- switch-case.swift
+++ switch-case.swift
@@ -4,13 +4,11 @@ let password = "iampassword"
 let authentication = (name: "twostraws", password:"iampassword", ipAddress: "127.0.0.1")
 
 switch authentication {
-case(_,_,_):
-    print("you caould be anyone")
 case ("bilbo", "iampassword",_):
     print("Hello, Bilbo")
 
-case ("twostraws", "iampassword", _):
-    print("Hello, twostraws !")
+case ("twostraws", "iampassword", let ip):
+    print("Hello, twostraws ! your ip adress is \(ip)")
     
 default: 
     print("Authentication fails !")  
