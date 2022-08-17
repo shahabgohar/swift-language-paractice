fd613288bee79b03d5bcd1cd35535c2b562cdef0
shahabgohar <shahab.developer.work@gmail.com>
2022-08-18 00:07:46 +0500

    get values in case for looop

--- loops/loops.swift
+++ loops/loops.swift
@@ -4,6 +4,6 @@ let taylor = (name: "taylor", password: "fr0st1es")
 
 let users = [twoStraws, bilbo, taylor]
 
-for case("shahab","iampassword") in users {
-    print("user found with the name shahab")
+for case let(name, password) in users {
+    print("user found with the name \(name) with password \(password)")
 }
