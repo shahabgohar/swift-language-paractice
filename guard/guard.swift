9f085b78d6161eecb9985549f54de482c86fcd1b
shahabgohar <shahab.developer.work@gmail.com>
2022-08-20 03:28:36 +0500

    early exit if conditions does not meet

---
+++ guard/guard.swift
@@ -0,0 +1,13 @@
+func giveAwards(name: String) {
+    guard name == "shahab" else{
+        print("No way !")
+        return
+    }
+
+    print("congratulations \(name) !")
+    
+}
+
+giveAwards(name: "shahab")
+
+giveAwards(name: "temp")
