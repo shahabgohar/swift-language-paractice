d9c478014f76739a5ddaeddd29ad7c7c1398718e
shahabgohar <shahab.developer.work@gmail.com>
2022-08-21 20:03:57 +0500

    string to int into string

--- types/initializers/initializers.swift
+++ types/initializers/initializers.swift
@@ -10,3 +10,13 @@ print(equalsArray)
 var board = [[String]](repeating: [String](repeating: "=", count: heading.count), count: 10)
 
 print(board)
+
+let int1 = 20
+let str1 = String(int1)
+
+let str2 = "something"
+
+let int2 = Int(str2) ?? 0
+
+print(int2)
+// true
